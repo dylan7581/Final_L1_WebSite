a4a0c52d83249496d8f6a7f55440b67f653cd30b
Luupz <39123341+Luupz@users.noreply.github.com>
2018-05-09 14:09:43 +0200

    Update Connexion.php

--- Connexion.php
+++ Connexion.php
@@ -4,22 +4,24 @@
 		include("sql_connect.php");
 		echo "<form method='post' action=""";
 		echo "<p>
-				<label for='id'>Identifiant</label> 
-				<input type='text' name='id' id='id' />
-			  </p>
-			  <p>
-				<label for='mdp'>Mot de passe</label> 
-				<input type='password' name='mdp' id='mdp' />
-			  </p>";
+			<label for='id'>Identifiant</label> 
+			<input type='text' name='id' id='id' />
+		     </p>
+		     <p>
+			<label for='mdp'>Mot de passe</label> 
+			<input type='password' name='mdp' id='mdp' />
+		     </p>";
 		echo "</form>";
 		$id = $_POST['id'];
 		$mdp = $_POST['mdp'];
-		$requete = "SELECT id FROM Users WHERE id = '$id' ";
+		$requete = "SELECT * FROM Users WHERE id = '$id' ";
 		$connect = mysqli_query($connexion, $requete);
+		mysqli_close($connexion);
 	}
 	else if(isset($_POST['Inscri']{
 		include("sql_connect.php");
 		echo "";
+		mysqli_close($connexion);
 	else{
 		echo "<p>Erreur</p>";
 	}
