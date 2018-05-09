8e1b511b15a4164fde96a9c7a603b24582afd3e7
Luupz <39123341+Luupz@users.noreply.github.com>
2018-05-09 15:21:24 +0200

    Update Publication.php

--- Publication.php
+++ Publication.php
@@ -1,1 +1,38 @@
-/*Page qui permet de publier de nouvelle annonce*/
+<?php
+/*Page qui permet de publier de nouvelles annonces*/
+?>
+
+<!DOCTYPE hmtl>
+
+  <html>
+	  <head>
+		  <title>Ajouter une annonce</title>
+		  <meta charset="utf-8" />
+	  </head>
+	  <body>
+		  <form method="post" action="">
+			  <p>
+				<input type="text" name="title" value="Titre (30 caractères maximum)" maxlength="30" />
+				<br/>
+			</p>
+			<p>
+				<input type="text" name="description" value="Description(400 caractères maximum)" maxlength="400" />
+				<br/>
+			</p>
+			<p>
+				<input type="submit" name="post" value="Publier"/>
+			</p>
+		  </form>
+	  </body>
+  </html>
+  
+  <?php
+	if(isset($_POST['post']){
+		$title = $_POST['title'];
+		$description = $_POST['description'];
+		$requete = "INSERT INTO article VALUES ('$title','$description')";
+		include("sql_connect.php");
+		mysqli_query($connexion, $requete);
+		mysqli_close($connexion);
+	}
+  ?>
