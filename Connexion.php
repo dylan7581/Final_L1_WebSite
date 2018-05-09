85dbcb1af5e39125c854717b321f1b2e00629c76
Luupz <39123341+Luupz@users.noreply.github.com>
2018-05-09 14:06:19 +0200

    Add files via upload

--- Connexion.php
+++ Connexion.php
@@ -1,1 +1,26 @@
-/*Page qui permet de s'inscrire ou de s'identifier*/
+<?php
+/*Page qui permet de s'inscrire ou de s'identifier*/ 
+	if(isset($_POST['Connect']{
+		include("sql_connect.php");
+		echo "<form method='post' action=""";
+		echo "<p>
+				<label for='id'>Identifiant</label> 
+				<input type='text' name='id' id='id' />
+			  </p>
+			  <p>
+				<label for='mdp'>Mot de passe</label> 
+				<input type='password' name='mdp' id='mdp' />
+			  </p>";
+		echo "</form>";
+		$id = $_POST['id'];
+		$mdp = $_POST['mdp'];
+		$requete = "SELECT id FROM Users WHERE id = '$id' ";
+		$connect = mysqli_query($connexion, $requete);
+	}
+	else if(isset($_POST['Inscri']{
+		include("sql_connect.php");
+		echo "";
+	else{
+		echo "<p>Erreur</p>";
+	}
+?>
