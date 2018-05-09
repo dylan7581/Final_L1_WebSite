57d6241a9b97a351f22f0954b44c4a065797c19b
Luupz <39123341+Luupz@users.noreply.github.com>
2018-05-09 14:35:52 +0200

    Update sql_connect.php

--- sql_connect.php
+++ sql_connect.php
@@ -1,7 +1,7 @@
 <?php
 /* remplacer :
  * "serveur" par le nom du serveur de bases de données
- * "login' par le l'identifiant sur le serveur
+ * "login" par le l'identifiant sur le serveur
  * "mot de passe" par le mot de passe associé
  * "base" par le nom de la base de données à utiliser
  */
