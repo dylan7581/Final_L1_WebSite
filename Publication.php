243d6f5c6322ad903df9915adee6027f26ce0644
Luupz <39123341+Luupz@users.noreply.github.com>
2018-05-09 16:22:04 +0200

    Update Publication.php

--- Publication.php
+++ Publication.php
@@ -15,11 +15,24 @@
 				<input type="text" name="title" value="Titre (30 caractères maximum)" maxlength="30" />
 				<br/>
 			</p>
+			  <select name="category">
+				  <option value="animal">Animaux</option>
+				  <option value="collection">Antiquités/Collection</option>
+				  <option value="housewear">Electroménager</option>
+				  <option value="work">Emploi/Formation</option>
+				  <option value="housing">Immobilier</option>
+				  <option value="home">Maison</option>
+				  <option value="materials">Matériel professionnel</option>
+				  <option value="beauty">Mode/Beauté</option>
+				  <option value="multimedia">Multimédia</option>
+				  <otpion value="hobbies">Loisirs</option>
+				  <option value="services">Services</option>
+				  <option value="vehicules">Véhicules</option>
 			<p>
 				<input type="text" name="description" value="Description(400 caractères maximum)" maxlength="400" />
 				<br/>
 			</p>
-			<p>
+			 <p>
 				<input type="submit" name="post" value="Publier"/>
 			</p>
 		  </form>
