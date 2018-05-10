c3969b5b025351a29e0db2c04660e656c31c0983
Luupz <39123341+Luupz@users.noreply.github.com>
2018-05-10 22:31:04 +0200

    Update Recherche.php

--- Recherche.php
+++ Recherche.php
@@ -1,7 +1,7 @@
 <?php
 /*Page qui permet de rechercher, elle souvre lorsqu'on recherche ou lorsqu'on clique sur une des catégories*/
   session_start();
-  if ($_SESSION["confirm"] == 1){
+  if (isset($_SESSION["confirm"])){
     include("modelco.inc.php");
   }
   else{
