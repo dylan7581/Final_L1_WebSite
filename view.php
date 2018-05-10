db967e35348258caf3d33d3f910bf1447da58c1b
Luupz <39123341+Luupz@users.noreply.github.com>
2018-05-10 22:15:44 +0200

    Update view.php

--- view.php
+++ view.php
@@ -1,7 +1,7 @@
 <!-- Page qui permet de voir un article -->
 <?php
   session_start();
-  if ($_SESSION['$confirm'] == 1)){
+  if ($_SESSION["confirm"] == 1)){
     include("modelco.inc.php");
   }
   else{
