729c3a264312f09d65f8e8d4bb7142b1e146dda6
Luupz <39123341+Luupz@users.noreply.github.com>
2018-05-10 22:04:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 	session_start();
-	if (isset($_SESSION['connect'])){
-   	   include("modelco.inc.php");
-	}
-	else{
-    	   include("model.inc.php");
-	}
+	if ($_SESSION['confirm'] == 1)){
+  	  include("modelco.inc.php");
+ 	 }
+ 	 else{
+  	  include("model.inc.php");
+ 	 } 
 	echo "<title>Adventice - Site d'annonce français!</title>";
 	echo "<h1 align='center'>Bienvenue sur le site d'annonce Adventice</h1>";
 ?>
