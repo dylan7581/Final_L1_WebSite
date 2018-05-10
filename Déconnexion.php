5f6157334643367c73291ddc859939c4afbb275f
dylan7581 <dylan.e@hotmail.fr>
2018-05-10 22:17:50 +0200

    modif

--- Déconnexion.php
+++ Déconnexion.php
@@ -2,7 +2,7 @@
 /*Page qui permet de se déconnecter*/
 	session_start();
 	include("modelco.inc.php");
-	if(isset($_POST['disconnect']){
+	if(isset($_POST['disconnect'])){
 		include("model.inc.php");
 	}else{
 		echo "<p> Erreur : déconnexion impossible </p>";
