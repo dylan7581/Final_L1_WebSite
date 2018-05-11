0315ef85e4332cf9896f8942c56b04ee9e435d99
Luupz <39123341+Luupz@users.noreply.github.com>
2018-05-11 18:30:48 +0200

    Update view.php

--- view.php
+++ view.php
@@ -14,6 +14,7 @@ $connect = mysqli_query($connexion, $requete);
 echo mysqli_error($connexion);
 $info = mysqli_fetch_array($connect);
 echo mysqli_error($connexion);
+echo $info;
 echo "<p>$info[2]</p>";
 echo "<title>$info[1]</title>";
 echo "<h2 id='titre'>$info[1]</h2>";
