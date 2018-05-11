1b01b2a7cb01445fea6abb992449315bad88c376
Luupz <39123341+Luupz@users.noreply.github.com>
2018-05-11 18:29:14 +0200

    Update view.php

--- view.php
+++ view.php
@@ -8,12 +8,12 @@
     include("model.inc.php");
     $_SESSION['username'] = "anonyme";
   }
-$title = $_GET['article'];
-echo $title;
+$title = $_GET['article'];;
 $requete = "SELECT * FROM article WHERE titre = '$title'";
 $connect = mysqli_query($connexion, $requete);
 echo mysqli_error($connexion);
 $info = mysqli_fetch_array($connect);
+echo mysqli_error($connexion);
 echo "<p>$info[2]</p>";
 echo "<title>$info[1]</title>";
 echo "<h2 id='titre'>$info[1]</h2>";
