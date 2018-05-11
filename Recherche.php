2fe4f99bc6618676c42e0e1aed9c62746964128f
Luupz <39123341+Luupz@users.noreply.github.com>
2018-05-11 15:43:23 +0200

    Update Recherche.php

--- Recherche.php
+++ Recherche.php
@@ -52,12 +52,15 @@
   if(isset($category)){
     include("sql_connect.php");
     if($category == $_POST['searchbar']){
-      $category = addslashes($category);
       $requete = "SELECT * FROM article WHERE titre LIKE '%$category%' OR categories = '$category' OR description LIKE '%$category%'";
       $resultat = mysqli_query($connexion, $requete);
       echo mysqli_error($connexion);
-      $article_affiche = mysqli_fetch_array($resultat);
-      echo "<p>$article_affiche[1]</p>";
+      $nrows = mysqli_num_rows($resultat)
+      echo "<p>$nrows résultats compatibles</p>";
+      for($k = 0; $k < $nrows, $k++){
+        $article = mysqli_fetch_array($resultat);
+        echo "<p>$article[1]</p>";
+      }
     }
     else{
       $requete = "SELECT * FROM article WHERE categories = '$category'";
@@ -65,9 +68,9 @@
       echo mysqli_error($connexion);
       /*$article_affiche = mysqli_fetch_array($resultat);
       echo "<p>$article_affiche[1]</p>"; */
-      $nrows = mysqli_num_rows($resultat);
+      $nrows = mysqli_num_rows($resultat)
       echo "<p>$nrows résultats compatibles</p>";
-      for($k = 0; $k < $nrows; $k++){
+      for($k = 0; $k < $nrows, $k++){
         $article = mysqli_fetch_array($resultat);
         echo "<p>$article[1]</p>";
       }
