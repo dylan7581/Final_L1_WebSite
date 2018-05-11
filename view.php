69c019777efcf5fae47b2b2b628a5e0f8f491b7a
Luupz <39123341+Luupz@users.noreply.github.com>
2018-05-11 16:04:29 +0200

    Update view.php

--- view.php
+++ view.php
@@ -12,6 +12,8 @@ $connect = mysqli_query($connexion, $requete);
 $info = mysqli_fetch_array($connect);
 echo "<p>$info[2]</p>";
 echo "<title>$info[1]</title>";
-echo "<h2>$info[1]</h2>";
-
+echo "<h2 id="title">$info[1]</h2>";
+echo "<div id="description">";
+echo "<p>$info[3]</p>";
+echo "</div>";
 ?>
